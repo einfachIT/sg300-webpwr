4684061abd2299e3ca8df2b3174b01caf5499e0f
Bastian Bukatz <bastian.greuel@gmail.com>
2024-05-15 16:25:39 +0200

    fix build error

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
   name='PIaaS',
   version='0.1',
+  long_description=open('README.md').read(),
+  long_description_content_type='text/markdown',
   packages=find_packages(),
   install_requires=[
   'Flask',
